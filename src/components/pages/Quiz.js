c404efe1a5fd4473ad7564433823525b53e04aa5
unknown <jirrazu123@gmail.com>
2022-08-29 17:07:20 +0600

    lesson-10 questions and questions check

--- src/components/pages/Quiz.js
+++ src/components/pages/Quiz.js
@@ -1,15 +1,74 @@
 import Answers from "../Answers";
 import ProgressBar from "../ProgressBar";
 import MiniPlayer from "../MiniPlayer";
+import { useParams } from "react-router-dom";
+import { useEffect, useState, useReducer } from "react";
+import useQuestions from "../../hooks/useQuestions";
+import _ from "lodash";
+
+const initialState = null;
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "questions":
+      action.value.forEach((question) => {
+        question.options.forEach((option) => {
+          option.chesked = false;
+        });
+      });
+      return action.value;
+
+    case "answer":
+      const questions = _.cloneDeep(state);
+      questions[action.questionID].options[action.optionIndex].checked =
+        action.value;
+
+      return questions;
+
+    default:
+      return state;
+  }
+};
 
 export default function Quiz() {
+  const { id } = useParams();
+  const { loading, error, questions } = useQuestions(id);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+
+  const [qna, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    dispatch({
+      type: "questions",
+      value: questions,
+    });
+  }, [questions]);
+
+  function handleAnswerChange(e, index) {
+    dispatch({
+      type: "answer",
+      questionID: currentQuestion,
+      optionIndex: index,
+      value: e.target.checked,
+    });
+  }
+
   return (
     <>
-      <h1>Pick three of your favorite Star Wars Flims</h1>
-      <h4>Question can have multiple answers</h4>
-      <Answers />
-      <ProgressBar />
-      <MiniPlayer />
+      {loading && <div>loading .... </div>}
+      {error && <div> There was an error!</div>}
+      {!loading && !error && qna && qna.length > 0 && (
+        <>
+          <h1>{qna[currentQuestion].title}</h1>
+          <h4>Question can have multiple answers</h4>
+          <Answers
+            options={qna[currentQuestion].options}
+            handlechange={handleAnswerChange}
+          />
+          <ProgressBar />
+          <MiniPlayer />
+        </>
+      )}
     </>
   );
 }
