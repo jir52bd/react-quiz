afa3c6d61beaaa2d13d4addd74a717ce6be60147
unknown <jirrazu123@gmail.com>
2022-08-29 11:27:23 +0600

    Lesson-9 InfiniteScroll add

--- src/hooks/useVideoList.js
+++ src/hooks/useVideoList.js
@@ -1,16 +1,30 @@
 import { useEffect, useState } from "react";
-import { getDatabase, ref, query, orderByKey, get } from "firebase/database";
+import {
+  getDatabase,
+  ref,
+  query,
+  orderByKey,
+  get,
+  startAt,
+  limitToFirst,
+} from "firebase/database";
 
-export default function useVideoList() {
+export default function useVideoList(page) {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [videos, setvideos] = useState([]);
+  const [hasMore, setHasMore] = useState(true);
   useEffect(() => {
     async function fetchVideos() {
       //database related works
       const db = getDatabase();
       const videosRef = ref(db, "videos");
-      const videoQuery = query(videosRef, orderByKey());
+      const videoQuery = query(
+        videosRef,
+        orderByKey(),
+        startAt("" + page),
+        limitToFirst(3)
+      );
 
       try {
         setError(false);
@@ -23,7 +37,7 @@ export default function useVideoList() {
             return [...prevVideos, ...Object.values(snapshort.val())];
           });
         } else {
-          //
+          setHasMore(false);
         }
       } catch (err) {
         console.log(err);
@@ -33,11 +47,12 @@ export default function useVideoList() {
     }
 
     fetchVideos();
-  }, []);
+  }, [page]);
 
   return {
     loading,
     error,
     videos,
+    hasMore,
   };
 }
